aae15c73197acbc5fda8de6e6e3bccdb7ef9893f
rafael kasinski <rakasin@gmail.com>
2023-03-14 17:18:52 -0300

    Fixed format button in display_results

--- project2/app/javascript/display_results.js
+++ project2/app/javascript/display_results.js
@@ -1,9 +1,25 @@
 window.addEventListener("DOMContentLoaded", () => {
   const type = new URLSearchParams(window.location.search).get("type")
+  const toggleButton = document.querySelector("#toggle-format")
+  let formatted = false
+
+  const displayData = (data) => {
+    if (formatted) {
+      document.querySelector("#json-results").innerText = JSON.stringify(data, null, 2)
+    } else {
+      document.querySelector("#json-results").innerText = JSON.stringify(data)
+    }
+  }
 
   fetch(`http://localhost:3000/interview/index?type=${type}`)
     .then((response) => response.json())
     .then((data) => {
-      document.getElementById("json-results").innerText = JSON.stringify(data)
+      displayData(data)
+
+      toggleButton.addEventListener("click", () => {
+        formatted = !formatted
+        displayData(data)
+        toggleButton.innerText = formatted ? "Raw" : "Formatted"
+      })
     })
 })
