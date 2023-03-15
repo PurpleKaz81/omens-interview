454813afdc0417053c3efad300eaca0807a4e6d2
rafael kasinski <rakasin@gmail.com>
2023-03-15 16:45:24 -0300

    Chenged to controller. Erased commenetd lines, fixed private method for returning falsy, content_type is json

--- project1/app/controllers/interview_controller.rb
+++ project1/app/controllers/interview_controller.rb
@@ -1,3 +1,5 @@
+require 'net/http'
+
 class InterviewController < ApplicationController
   def index
     url = 'https://api.omens.com.br/single-item/open'
@@ -10,29 +12,14 @@ class InterviewController < ApplicationController
       program.slice('name', 'price', 'variantId', 'type')
     end
 
-    render json: output
+    render json: output, content_type: 'application/json'
   end
 
   private
 
   def program_type
+    return false if params[:type] == 'all'
+
     params[:type]
   end
 end
-
-
-
-# iterate over an array of hashes
-# each hash has keys name, price, variantId, type. Some have presentation
-# my concern is type
-# type has values course and phyto
-
-# return an array with key type and phyto value
-# build object output = { type: phyto }
-
-# obs
-# return filtered hashto return phyto
-# remove course and psycho and return full hash with phyto, not just the type, bring 4 key-values but only the ones where type: phyto
-# .map is not best solution, find more optimal one
-# push to new repo on GH, make public
-# Plus: put filter on query, such as a param for the query. End point is interview/index?type=phyto. Do this in router
